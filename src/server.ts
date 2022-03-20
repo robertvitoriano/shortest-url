3e8aaae21385c258446874a34e67e7c1d885f3e9
Robert Vitoriano <robertvitoriano@gmail.com>
2022-03-20 17:20:36 -0300

    change PORT variable import

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import { app } from './app'
 
-app.listen(process.env.PORT || 3333,()=>{
-  console.log('My application is running '+ process.env.PORT)
+const {variables } = require('./../config/variables')
+
+app.listen(variables.PORT || 3333,()=>{
+  console.log('My application is running '+ variables.PORT)
 })
